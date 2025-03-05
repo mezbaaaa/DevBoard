99a8c40b0851e41ea5a7377082c761d0b353f52d
mezbaaaa <tahsinul101@gmail.com>
2025-03-06 00:15:31 +0600

    scripts added

---
+++ script/buttonIssue.js
@@ -0,0 +1,29 @@
+document
+  .getElementById("button-issue-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const totalTaskNumber =
+      document.getElementById("total-task-number").innerText;
+    const convertedTotalTaskNumber = parseInt(totalTaskNumber);
+    const taskNumber = document.getElementById("task-number").innerText;
+    const convertedTaskNumber = parseInt(taskNumber);
+    const buttonIssue = document.getElementById("button-issue-btn");
+
+    alert("Board upgraded successfully!");
+    buttonIssue.setAttribute("disabled", true);
+    buttonIssue.classList.add(
+      "bg-gray-300",
+      "text-gray-500",
+      "cursor-not-allowed"
+    );
+
+    let number = 1;
+    const difference = convertedTaskNumber - number;
+    document.getElementById("task-number").innerText = difference;
+    const sum = convertedTotalTaskNumber + number;
+    document.getElementById("total-task-number").innerText = sum
+
+  });
+
+
---
+++ script/openAi.js
@@ -0,0 +1,27 @@
+document
+  .getElementById("openai-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const totalTaskNumber =
+      document.getElementById("total-task-number").innerText;
+    const convertedTotalTaskNumber = parseInt(totalTaskNumber);
+    const taskNumber = document.getElementById("task-number").innerText;
+    const convertedTaskNumber = parseInt(taskNumber);
+    const buttonIssue = document.getElementById("openai-btn");
+
+    alert("Board upgraded successfully!");
+    buttonIssue.setAttribute("disabled", true);
+    buttonIssue.classList.add(
+      "bg-gray-300",
+      "text-gray-500",
+      "cursor-not-allowed"
+    );
+
+    let number = 1;
+    const difference = convertedTaskNumber - number;
+    document.getElementById("task-number").innerText = difference;
+    const sum = convertedTotalTaskNumber + number;
+    document.getElementById("total-task-number").innerText = sum
+
+  });
